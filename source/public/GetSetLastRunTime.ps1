1af575c95d9de0e93e6d935cda2bddde9a8b9664
June Castillote <june.castillote@gmail.com>
2021-01-28 22:58:33 +0800

    update

---
+++ source/public/GetSetLastRunTime.ps1
@@ -0,0 +1,46 @@
+Function Set-MS365HealthReportLastRunTime {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$TenantID,
+
+        [parameter()]
+        [datetime]$LastRunTime
+    )
+    $now = Get-Date
+    $RegPath = "HKCU:\Software\MS365HealthReport\$TenantID"
+
+    $regSplat = @{
+        Path  = $RegPath
+        Value = $(
+            if ($LastRunTime) {
+                $LastRunTime
+            }
+            else {
+                $now
+            }
+        )
+    }
+    $null = New-Item @regSplat -Force
+}
+
+Function Get-MS365HealthReportLastRunTime {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$TenantID
+    )
+    $now = Get-Date
+    $RegPath = "HKCU:\Software\MS365HealthReport\$TenantID"
+
+    try {
+        $value = Get-ItemPropertyValue -Path $RegPath -Name "(default)" -ErrorAction Stop
+        return $value
+    }
+    catch {
+        Set-MS365HealthReportLastRunTime -TenantID $TenantID -LastRunTime $now
+        return $now
+    }
+}
--- source/public/newM365IncidentReport.ps1
+++ source/public/newM365IncidentReport.ps1
@@ -1,33 +1,37 @@
 Function New-MS365IncidentReport {
-    [cmdletbinding(DefaultParameterSetName = 'bySecret')]
+    [cmdletbinding(DefaultParameterSetName = 'Client Secret')]
     param (
         [parameter()]
         [string]
         $OrganizationName,
 
-        [parameter(Mandatory, ParameterSetName = 'byCertificate')]
-        [parameter(Mandatory, ParameterSetName = 'byThumbprint')]
-        [parameter(Mandatory, ParameterSetName = 'bySecret')]
+        [parameter(Mandatory, ParameterSetName = 'Client Certificate')]
+        [parameter(Mandatory, ParameterSetName = 'Certificate Thumbprint')]
+        [parameter(Mandatory, ParameterSetName = 'Client Secret')]
         [guid]
         $ClientID,
 
-        [parameter(Mandatory, ParameterSetName = 'bySecret')]
+        [parameter(Mandatory, ParameterSetName = 'Client Secret')]
         [string]
         $ClientSecret,
 
-        [parameter(Mandatory, ParameterSetName = 'byCertificate')]
+        [parameter(Mandatory, ParameterSetName = 'Client Certificate')]
         [System.Security.Cryptography.X509Certificates.X509Certificate2]
         $ClientCertificate,
 
-        [parameter(Mandatory, ParameterSetName = 'byThumbprint')]
+        [parameter(Mandatory, ParameterSetName = 'Certificate Thumbprint')]
         [string]
         $ClientCertificateThumbprint,
 
-        [parameter(Mandatory, ParameterSetName = 'byCertificate')]
-        [parameter(Mandatory, ParameterSetName = 'byThumbprint')]
-        [parameter(Mandatory, ParameterSetName = 'bySecret')]
+        [parameter(Mandatory, ParameterSetName = 'Client Certificate')]
+        [parameter(Mandatory, ParameterSetName = 'Certificate Thumbprint')]
+        [parameter(Mandatory, ParameterSetName = 'Client Secret')]
         [string]
         $TenantID,
+
+        [Parameter()]
+        [switch]
+        $StartFromLastRun,
 
         [Parameter()]
         [datetime]
@@ -42,66 +46,120 @@ Function New-MS365IncidentReport {
         $SendEmail,
 
         [Parameter()]
-        [mailaddress]
+        [string]
         $From,
 
         [Parameter()]
-        [mailaddress[]]
+        [string[]]
         $To,
 
         [Parameter()]
-        [mailaddress[]]
+        [string[]]
         $CC,
 
         [Parameter()]
-        [mailaddress[]]
-        $Bcc
-    )
+        [string[]]
+        $Bcc,
 
+        # Parameter help description
+        [Parameter()]
+        [boolean]
+        $WriteReportToDisk = $true
+    )
     $ModuleInfo = Get-Module MS365HealthReport
+    $Now = Get-Date
     if (!$OrganizationName) { $OrganizationName = $TenantID }
 
-    Write-Verbose $($pscmdlet.ParameterSetName)
+    Write-Output "Authentication type: $($pscmdlet.ParameterSetName)"
+    Write-Output "Client ID: $ClientID"
+    Write-Output "Tenant ID: $TenantID"
 
-    if ($pscmdlet.ParameterSetName -eq 'bySecret') {
+    # Get Service Communications API Token
+    if ($pscmdlet.ParameterSetName -eq 'Client Secret') {
         $SecureClientSecret = New-Object System.Security.SecureString
         $ClientSecret.toCharArray() | ForEach-Object { $SecureClientSecret.AppendChar($_) }
         $ServiceCommsApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientSecret $SecureClientSecret -TenantId $tenantID -Scopes 'https://manage.office.com/.default' -ErrorAction Stop
     }
-    elseif ($pscmdlet.ParameterSetName -eq 'byCertificate') {
+    elseif ($pscmdlet.ParameterSetName -eq 'Client Certificate') {
         $ServiceCommsApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientCertificate $ClientCertificate -TenantId $tenantID -Scopes 'https://manage.office.com/.default' -ErrorAction Stop
     }
-    elseif ($pscmdlet.ParameterSetName -eq 'byThumbprint') {
+    elseif ($pscmdlet.ParameterSetName -eq 'Certificate Thumbprint') {
         $ServiceCommsApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientCertificate (Get-Item Cert:\CurrentUser\My\$($ClientCertificateThumbprint)) -TenantId $tenantID -Scopes 'https://manage.office.com/.default' -ErrorAction Stop
     }
 
+    # Get GraphAPI Token
+    if ($SendEmail) {
+
+        if (!$From) { Write-Warning "You ask me to send an email report but you forgot to add the -From address."; return $null }
+        if (!$To) { Write-Warning "You ask me to send an email report but you forgot to add the -To address(es)."; return $null }
+
+        if ($pscmdlet.ParameterSetName -eq 'Client Secret') {
+            $SecureClientSecret = New-Object System.Security.SecureString
+            $ClientSecret.toCharArray() | ForEach-Object { $SecureClientSecret.AppendChar($_) }
+            $GraphApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientSecret $SecureClientSecret -TenantId $tenantID -Scopes @('https://graph.microsoft.com/.default') -ErrorAction Stop
+        }
+        elseif ($pscmdlet.ParameterSetName -eq 'Client Certificate') {
+            $GraphApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientCertificate $ClientCertificate -TenantId $tenantID -Scopes @('https://graph.microsoft.com/.default') -ErrorAction Stop
+        }
+        elseif ($pscmdlet.ParameterSetName -eq 'Certificate Thumbprint') {
+            $GraphApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientCertificate (Get-Item Cert:\CurrentUser\My\$($ClientCertificateThumbprint)) -TenantId $tenantID -Scopes @('https://graph.microsoft.com/.default') -ErrorAction Stop
+        }
+        $GraphAPIHeader = @{'Authorization' = "Bearer $($GraphApiOAuth2.AccessToken)" }
+    }
+
+    #Region Get Incidents
     $searchParam = @{
         Token       = ($ServiceCommsApiOAuth2.AccessToken);
         MessageType = 'Incident';
     }
 
+    ## If -StartFromLastRun, this function will only get the incidents whose LastUpdatedTime is after the timestamp in "HKCU:\Software\MS365HealthReport\$TenantID"
+    if ($StartFromLastRun) {
+        Write-Output "Getting last run time from the registry."
+        $LastUpdatedTime = Get-MS365HealthReportLastRunTime -TenantID $TenantID
+    }
+
+    ## If -LastUpdatedTime, this function will only get the incidents whose LastUpdatedTime is after the $LastUpdatedTime datetime value.
     if ($LastUpdatedTime) {
         $searchParam += (@{LastUpdatedTime = $LastUpdatedTime })
+        Write-Output "Getting incident reports from: $LastUpdatedTime"
     }
 
     if ($Workload) {
         $searchParam += (@{Workload = $Workload })
+        Write-Output "Workload: $($Workload -join ',')"
+    }
+    try {
+        $events = @(Get-MS365Messages @searchParam -ErrorAction STOP)
+        Write-Output "Total Incidents Retrieved: $($events.Count)"
+    }
+    catch {
+        Write-Output "Failed to get data. $($_.Exception.Message)"
+        return $null
     }
 
-    $events = @(Get-MS365Messages @searchParam)
-    Write-Verbose "Events=$($events.Count)"
-    #$events
+    #EndRegion
 
+    #Region Prepare Output Directory
+    if ($WriteReportToDisk -eq $true) {
+        $outputDir = "$($env:TMP)\$($ModuleInfo.Name)\$($TenantID)"
+        if (!(Test-Path -Path $outputDir)) {
+            $null = New-Item -ItemType Directory -Path $outputDir -Force
+        }
+        else {
+            Remove-Item -Path $outputDir\* -Recurse -Force -Confirm:$false
+        }
+        Write-Output "Output Directory: $outputDir"
+    }
+
+    #EndRegion
+
+    #Region Create Report
+
+    ## Get the CSS style
     $css_string = Get-Content (($ModuleInfo.ModuleBase.ToString()) + '\source\public\style.css') -Raw
-    $outputDir = "$($env:TMP)\$($ModuleInfo.Name)\$($TenantID)"
 
-    if (!(Test-Path -Path $outputDir)) {
-        $null = New-Item -ItemType Directory -Path $outputDir -Force
-    }
-    else {
-        Remove-Item -Path $outputDir\* -Recurse -Force -Confirm:$false
-    }
-
+    ## Process each incident
     if ($events.Count -gt 0) {
         foreach ($event in $events) {
             $event_id_file = "$outputDir\$($event.ID).html"
@@ -120,34 +178,36 @@ Function New-MS365IncidentReport {
             $null = $htmlBody.Add('<tr><th>Organization</th><td>' + $organizationName + '</td></tr>')
             $null = $htmlBody.Add('<tr><th>Classification</th><td>' + $event.Classification + '</td></tr>')
             $null = $htmlBody.Add('<tr><th>User Impact</th><td>' + $event.ImpactDescription + '</td></tr>')
-
             $null = $htmlBody.Add('<tr><th>Last Updated</th><td>' + [datetime]$event.LastUpdatedTime + '</td></tr>')
-
             $null = $htmlBody.Add('<tr><th>Start Time</th><td>' + [datetime]$event.StartTime + '</td></tr>')
-            if ($event.EndTime) {
-                $null = $htmlBody.Add('<tr><th>End Time</th><td>' + [datetime]$event.EndTime + '</td></tr>')
-            }
-            else {
-                $null = $htmlBody.Add('<tr><th>End Time</th><td></td></tr>')
-            }
+            $null = $htmlBody.Add('<tr><th>End Time</th><td>' + $(
+                    if ($event.EndTime) {
+                        [datetime]$event.EndTime
+                    }
+                    else {
+                        ""
+                    }
+                ) + '</td></tr>')
 
-            $null = $htmlBody.Add('<tr><th>Latest Message</th><td>' + ($event.Messages[-1].MessageText).Replace("`n", "<br />").Replace('�','') + '</td></tr>')
+            $null = $htmlBody.Add('<tr><th>Latest Message</th><td>' + ($event.Messages[-1].MessageText).Replace("`n", "<br />") + '</td></tr>')
             $null = $htmlBody.Add('</table>')
 
-            $null = $htmlBody.Add('<p><table id="section">')
+            #$null = $htmlBody.Add('<p><table id="section">')
             $null = $htmlBody.Add('<p><font size="2" face="Segoe UI Light"><br />')
             $null = $htmlBody.Add('<br />')
             $null = $htmlBody.Add('<a href="' + $ModuleInfo.ProjectURI.ToString() + '" target="_blank">' + $ModuleInfo.Name.ToString() + ' v' + $ModuleInfo.Version.ToString() + ' </a><br>')
             $null = $htmlBody.Add('</body>')
             $null = $htmlBody.Add('</html>')
             $htmlBody = $htmlBody -join "`n" #convert to multiline string
-            $htmlBody | Out-File $event_id_file -Force
+
+            if ($WriteReportToDisk -eq $true) {
+                $htmlBody | Out-File $event_id_file -Force
+            }
+
 
             if ($SendEmail -eq $true) {
-                if (!$From) { Write-Warning "You ask me to send an email report but you forgot to add the -From address."; return $null }
-                if (!$To) { Write-Warning "You ask me to send an email report but you forgot to add the -To address(es)."; return $null }
 
-                #recipients
+                # Recipients
                 $toAddressJSON = @()
                 $To | ForEach-Object {
                     $toAddressJSON += @{EmailAddress = @{Address = $_ } }
@@ -174,31 +234,42 @@ Function New-MS365IncidentReport {
                         }
                     }
 
+                    ## Add CC recipients if specified
+                    if ($Cc) {
+                        $ccAddressJSON = @()
+                        $Cc | ForEach-Object {
+                            $ccAddressJSON += @{EmailAddress = @{Address = $_ } }
+                        }
+                        $mailBody.Message += @{ccRecipients = $ccAddressJSON }
+                    }
+
+                    ## Add BCC recipients if specified
+                    if ($Bcc) {
+                        $BccAddressJSON = @()
+                        $Bcc | ForEach-Object {
+                            $BccAddressJSON += @{EmailAddress = @{Address = $_ } }
+                        }
+                        $mailBody.Message += @{BccRecipients = $BccAddressJSON }
+                    }
+
+                    ## Send email
                     $ServicePoint = [System.Net.ServicePointManager]::FindServicePoint('https://graph.microsoft.com')
-
-                    # Get GraphAPI Token
-                    if ($pscmdlet.ParameterSetName -eq 'bySecret') {
-                        $SecureClientSecret = New-Object System.Security.SecureString
-                        $ClientSecret.toCharArray() | ForEach-Object { $SecureClientSecret.AppendChar($_) }
-                        $GraphApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientSecret $SecureClientSecret -TenantId $tenantID -Scopes @('https://graph.microsoft.com/Mail.Send') -ErrorAction Stop
-                    }
-                    elseif ($pscmdlet.ParameterSetName -eq 'byCertificate') {
-                        $GraphApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientCertificate $ClientCertificate -TenantId $tenantID -Scopes @('https://graph.microsoft.com/Mail.Send') -ErrorAction Stop
-                    }
-                    elseif ($pscmdlet.ParameterSetName -eq 'byThumbprint') {
-                        $GraphApiOAuth2 = Get-MsalToken -ClientId $ClientID -ClientCertificate (Get-Item Cert:\CurrentUser\My\$($ClientCertificateThumbprint)) -TenantId $tenantID -Scopes @('https://graph.microsoft.com/Mail.Send') -ErrorAction Stop
-                    }
-
-                    $header = @{'Authorization' = "Bearer $($GraphApiOAuth2.AccessToken)" }
-
-                    $null = Invoke-RestMethod -Method Post -Uri "https://graph.microsoft.com/v1.0/users/$($From)/sendmail" -Body ($mailBody | ConvertTo-Json -Depth 4) -Headers $header -ContentType application/json
-
+                    Write-Host "Sending Alert for $($event.id)"
+                    $null = Invoke-RestMethod -Method Post -Uri "https://graph.microsoft.com/v1.0/users/$($From)/sendmail" -Body $($mailBody | ConvertTo-Json -Depth 4) -Headers $GraphAPIHeader -ContentType application/json
                     $null = $ServicePoint.CloseConnectionGroup('')
+
                 }
                 catch {
-                    Write-Error "Failed to send Alert for $($event.id). $($_.Exception.Message)"
+                    Write-Output "Failed to send Alert for $($event.id). $($_.Exception.Message)"
+                    return $null
                 }
             }
         }
+    }
+    #EndRegion
+
+    if ($StartFromLastRun) {
+        Write-Output "Setting last run time in the registry to $Now"
+        Set-MS365HealthReportLastRunTime -TenantID $tenantID -LastRunTime $Now
     }
 }
